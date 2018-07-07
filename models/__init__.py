1f26a8d73a75a0058a284329e6b8308c36f207cb
wzn0828 <18001279185@163.com>
2018-07-07 14:38:29 +0800

    In __init__.py, add opt.load_best to judge whether load previous best model or previous last model

--- models/__init__.py
+++ models/__init__.py
@@ -63,6 +63,9 @@ def setup(opt):
         # check if all necessary files exist 
         assert os.path.isdir(opt.start_from)," %s must be a a path" % opt.start_from
         assert os.path.isfile(os.path.join(opt.start_from,"infos_"+opt.id+".pkl")),"infos.pkl file does not exist in path %s"%opt.start_from
-        model.load_state_dict(torch.load(os.path.join(opt.start_from, 'model.pth')))
+        if opt.load_best:
+            model.load_state_dict(torch.load(os.path.join(opt.start_from, 'model-best.pth')))
+        else:
+            model.load_state_dict(torch.load(os.path.join(opt.start_from, 'model.pth')))
 
     return model
