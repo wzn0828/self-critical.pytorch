41775bf466714de65e413bbe122219eea5499f19
wzn0828 <18001279185@163.com>
2018-09-07 16:43:03 +0800

    In opts, change the default train_eval_images_use to 1000

--- opts.py
+++ opts.py
@@ -126,7 +126,7 @@ def parse_opt():
                     help='Do we load previous best score when resuming training.')
     parser.add_argument('--load_best', type=int, default=0,
                     help='Do we load previous best model? (0 = disable)')
-    parser.add_argument('--train_eval_images_use', type=int, default=480,
+    parser.add_argument('--train_eval_images_use', type=int, default=1000,
                     help='how many images to use when periodically evaluating the train performance?')
 
 
