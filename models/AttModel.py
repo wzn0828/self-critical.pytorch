bf87d22d0efeafa497b82b6d06bf44f054fbcd53
wzn0828 <18001279185@163.com>
2018-11-02 19:30:27 +0800

    Change the std initialization of word_embedding from 1 to 0.2

--- models/AttModel.py
+++ models/AttModel.py
@@ -118,7 +118,7 @@ class AttModel(CaptionModel):
             self.ln_out_embedding = nn.LayerNorm(self.rnn_size)
 
         # initialization
-        nn.init.normal_(self.embed[0].weight, mean=0, std=1)
+        nn.init.normal_(self.embed[0].weight, mean=0, std=0.2)
         model_utils.kaiming_normal('relu', 0, filter(lambda x: 'linear' in str(type(x)), self.fc_embed)[0],
                                    filter(lambda x: 'linear' in str(type(x)), self.att_embed)[0])
         model_utils.xavier_normal('tanh', self.ctx2att)
